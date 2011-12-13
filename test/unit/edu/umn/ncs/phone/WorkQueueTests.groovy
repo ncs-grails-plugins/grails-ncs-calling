1b4e21eff6c816144527e80aeea369c6e2687901
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2011-12-13 20:21:30 +0000

    fixing test syntax error
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs-grails-plugins/grails-ncs-calling/trunk@219 87bbd5ad-132b-4bfb-b67c-16c9f46f48d1

--- test/unit/edu/umn/ncs/phone/WorkQueueTests.groovy
+++ test/unit/edu/umn/ncs/phone/WorkQueueTests.groovy
@@ -2,7 +2,7 @@ package edu.umn.ncs.phone
 
 import grails.test.*
 
-class UserQueueTests extends GrailsUnitTestCase {
+class WorkQueueTests extends GrailsUnitTestCase {
     protected void setUp() {
         super.setUp()
     }
