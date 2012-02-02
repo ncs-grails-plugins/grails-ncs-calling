fec9589fee405126bd81bd3b4a52d0720611a196
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2012-02-02 19:54:29 +0000

    adding codenarc and code-coverage
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs-grails-plugins/grails-ncs-calling/trunk@237 87bbd5ad-132b-4bfb-b67c-16c9f46f48d1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -35,3 +35,14 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
 }
+codenarc.reports = {
+	JenkinsXmlReport('xml') {
+		outputFile = 'target/test-reports/CodeNarcReport.xml'
+		title = 'CodeNarc Report for NCS Calling Plugin'
+	}
+	JenkinsHtmlReport('html') {
+		outputFile = 'CodeNarcReport.html'
+		title = 'CodeNarc Report for NCS Calling Plugin'
+	}
+}
+codenarc.propertiesFile = 'grails-app/conf/codenarc.properties'
